cd03719a7f77f566ae79c1c954b2eb9da6d55946
w3kasper <hellovikram1@gmail.com>
2024-01-30 21:20:22 -0700

    message

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -20,14 +20,87 @@ const SearchBar = () => {
   };
 
   return (
-    <div>
-      <input type="text" value={input} onChange={handleInputChange} />
-      {results.map((result) => (
-        <div key={result.blog_id}>
-          <h2>{result.blog_title}</h2>
+    <>
+      {/* SEARCH INPUT */}
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <input
+            type="text"
+            value={input}
+            onChange={handleInputChange}
+            style={{
+              width: "550px",
+              height: "40px",
+              borderRadius: "20px",
+              padding: "10px",
+              paddingLeft: "20px",
+              fontSize: "16px",
+              backgroundColor: "#f0f0f0",
+              border: "2px solid rgb(82, 133, 196)",
+              outline: "none",
+            }}
+          />
+          <div
+            style={{
+              width: "40px",
+              height: "40px",
+              borderRadius: "50%",
+              backgroundColor: "#242424",
+              border: "2px solid rgb(82, 133, 196)",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginLeft: "10px",
+            }}
+          >
+            <img src="/search.svg" alt="Search" style={{ height: "20px" }} />
+          </div>
         </div>
-      ))}
-    </div>
+        {/* POP DOWN RESULTS */}
+        {results.length > 0 && (
+          <>
+            <div
+              style={{
+                marginTop: "10px",
+                marginRight: "50px",
+                width: "550px",
+                backgroundColor: "#f0f0f0",
+                border: "2px solid rgb(82, 133, 196)",
+                borderRadius: "20px",
+                color: "#242424",
+                paddingLeft: "10px",
+              }}
+            >
+              {results.map((result) => (
+                <div
+                  key={result.blog_id}
+                  style={{
+                    padding: "10px",
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <span>{result.blog_title}</span>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
